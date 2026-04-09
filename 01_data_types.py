3a4eb52b81245e1365676ad8f2062962daebc338
ANUJ SAHU <sahuganuj004@gmail.com>
2026-04-09 23:09:37 +0530

    Create 01_data_types.py

---
+++ 01_data_types.py
@@ -0,0 +1,11 @@
+# 01_data_types.py
+
+a = 10          # int
+b = 5.5         # float
+c = "Hello"     # string
+d = True        # boolean
+e = [1, 2, 3]   # list
+f = (1, 2, 3)   # tuple
+g = {"a": 1}    # dictionary
+
+print(type(a), type(b), type(c), type(d), type(e), type(f), type(g))
